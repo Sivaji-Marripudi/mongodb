965a726dae7b9b48657e83d2fe97179a4e211ff7
sivaji chowdary <sivajimarripudi98@gmail.com>
2020-06-13 10:48:20 +0530

    'updated'

--- mongo.py
+++ mongo.py
@@ -1,9 +1,9 @@
 import pymongo
 from pymongo import MongoClient
-from pprint import pprint
+from pprint import pprint # pprint library is used to make the output look more pretty
 client = MongoClient('localhost',27017)
 pprint(client)
-db = client.test
+db = client.test # created database 'test'
 db.sites.insert_one({'url':'https://www.python.org','name':'python documentation'})
 site = db.sites.find_one()
 pprint(site)
